3847be9c4fc4e0f5f8c886ace33f375956a7bce4
pavanmane1 <pavanmane4961@gmail.com>
2023-07-28 11:18:23 +0530

    Your commit message here

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:managment/Screens/home.dart';
-import 'package:managment/Screens/statistics.dart';
 import 'package:managment/widgets/bottomnavigationbar.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
